7193562a5810e8296885b6687ac4d04607c6a115
Joeyzeeee <krisna.adstr@gmail.com>
2024-09-06 09:55:54 +0700

    praktikum01

---
+++ Praktikum01/Latihan09.java
@@ -0,0 +1,15 @@
+// Latihan09
+
+public class Latihan09 {
+
+    public static void main(String[] args) {
+
+        System.out.println("**** Identitas diri****");
+        System.out.println("Nama     :  Krisna Aditya Satria A.");
+        System.out.println("Absen    :  09");
+        System.out.println("Kelas    :  1D");
+        System.out.println("Prodi    :  Sistem Infomasi Bisnis");
+        System.out.println("Jurusan  :  Teknologi Informasi");
+
+    }
+}
