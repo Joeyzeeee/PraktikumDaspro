8c4d5efcb037ec3f36043bb8768ccfe578e22518
Joeyzeeee <krisna.adstr@gmail.com>
2024-11-11 18:12:33 +0700

    Key

--- Praktikum09/SearchNilai09.java
+++ Praktikum09/SearchNilai09.java
@@ -8,20 +8,32 @@ public class SearchNilai09 {
 
         Scanner input09 = new Scanner(System.in);
 
-        int[] arrNilai = { 80, 85, 78, 96, 90, 82, 86 };
-        int key = 90;
-        int hasil = 0;
+        System.out.print("Masukkan banyaknya nilai yang diinput: ");
+        int banyakNilai = input09.nextInt();
+
+        int[] arrNilai = new int[banyakNilai];
+
+        for (int i = 0; i < banyakNilai; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            arrNilai[i] = input09.nextInt();
+        }
+
+        System.out.print("Masukkkan nilai yang dicari : ");
+        int key = input09.nextInt();
+        int hasil = -1;
 
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
-                hasil = i;
+                hasil = i + 1;
                 break;
 
             }
         }
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + hasil);
+        } else {
+            System.out.println("Nilai " + key + " tidak ditemukan.");
+        }
 
         input09.close();
     }
